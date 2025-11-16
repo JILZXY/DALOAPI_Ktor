1f8a683eb34c4d0a7f0561f6dbd5cb8d88873d2e
JILZXY <sasosiso@gmail.com>
2025-11-15 20:56:10 -0600

    Port de usuarios creado

---
+++ src/main/kotlin/usuario/domain/port/AbogadoServicePort.kt
@@ -0,0 +1,10 @@
+package com.example.usuario.domain.port
+
+import com.example.usuario.domain.model.Abogado
+
+interface AbogadoServicePort {
+    suspend fun getAllAbogados(): List<Abogado>
+    suspend fun getAbogadoById(id: String): Abogado?
+    suspend fun getAbogadosByEspecialidad(especialidadId: Int): List<Abogado>
+    suspend fun updateAbogado(id: String, abogado: Abogado): Abogado?
+}
