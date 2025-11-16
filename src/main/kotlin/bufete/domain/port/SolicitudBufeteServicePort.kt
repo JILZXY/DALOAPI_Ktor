5816e47cf230a751906d9456bc9c6d782d2b57b8
JILZXY <sasosiso@gmail.com>
2025-11-15 21:46:32 -0600

    solicitudBufete service

---
+++ src/main/kotlin/bufete/domain/port/SolicitudBufeteServicePort.kt
@@ -0,0 +1,13 @@
+package com.example.bufete.domain.port
+
+import com.example.bufete.domain.model.SolicitudBufete
+
+interface SolicitudBufeteServicePort {
+    suspend fun getAllSolicitudes(): List<SolicitudBufete>
+    suspend fun getSolicitudById(id: Int): SolicitudBufete?
+    suspend fun getSolicitudesByBufeteId(bufeteId: Int): List<SolicitudBufete>
+    suspend fun getSolicitudesByAbogadoId(abogadoId: String): List<SolicitudBufete>
+    suspend fun createSolicitud(abogadoId: String, bufeteId: Int): SolicitudBufete?
+    suspend fun aprobarSolicitud(id: Int): Boolean
+    suspend fun rechazarSolicitud(id: Int): Boolean
+}
