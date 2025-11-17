186dc526d673af0aee5dbac5fd5a48198e7822bf
Luissmont <luispm2424@gmail.com>
2025-11-16 18:23:33 -0600

    mensajes service

---
+++ src/main/kotlin/chat/domain/port/MensajeServicePort.kt
@@ -0,0 +1,10 @@
+package com.example.chat.domain.port
+
+import com.example.chat.domain.model.Mensaje
+
+interface MensajeServicePort {
+    suspend fun getMensajesByChatId(chatId: Int): List<Mensaje>
+    suspend fun getMensajeById(id: Int): Mensaje?
+    suspend fun sendMensaje(chatId: Int, remitenteId: String, mensaje: String): Mensaje?
+    suspend fun deleteMensaje(id: Int): Boolean
+}
