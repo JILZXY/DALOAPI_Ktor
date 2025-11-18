7c24d4b332a4a8ab653d77fe2a477d1edcdde83b
JILZXY <sasosiso@gmail.com>
2025-11-17 19:32:42 -0600

    Funciones implementadas en abogado servieces

--- src/main/kotlin/usuario/application/AbogadoService.kt
+++ src/main/kotlin/usuario/application/AbogadoService.kt
@@ -3,7 +3,7 @@ package com.example.usuario.application
 import com.example.usuario.domain.model.Abogado
 import com.example.usuario.domain.port.Service.AbogadoServicePort
 import com.example.usuario.domain.port.Repository.AbogadoRepositoryPort
-
+import com.example.shared.domain.model.Especialidad
 class AbogadoService(
     private val abogadoRepository: AbogadoRepositoryPort
 ) : AbogadoServicePort {
@@ -22,5 +22,31 @@ class AbogadoService(
 
     override suspend fun updateAbogado(id: String, abogado: Abogado): Abogado? {
         return abogadoRepository.update(abogado)
+    }
+
+    override suspend fun buscarAbogadosPorNombre(nombre: String): List<Abogado> {
+        return abogadoRepository.findActivosByNombre(nombre)
+    }
+
+    override suspend fun getAbogadosByLocalidad(estadoId: Int?, municipioId: Int?): List<Abogado> {
+        return abogadoRepository.findActivosByLocalidad(estadoId, municipioId)
+    }
+
+    override suspend fun filtrarAbogados(
+        materiaId: Int?,
+        estadoId: Int?,
+        municipioId: Int?,
+        ordenarPorCalificacion: Boolean
+    ): List<Abogado> {
+        return abogadoRepository.findActivosConFiltros(
+            materiaId,
+            estadoId,
+            municipioId,
+            ordenarPorCalificacion
+        )
+    }
+
+    override suspend fun getEspecialidadesByAbogadoId(abogadoId: String): List<Especialidad> {
+        return abogadoRepository.getEspecialidadesByAbogadoId(abogadoId)
     }
 }
