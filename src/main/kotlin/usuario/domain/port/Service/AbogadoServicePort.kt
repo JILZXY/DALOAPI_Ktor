85a6c2ef37a5803c0d053afcc9d540f23d6f6951
JILZXY <sasosiso@gmail.com>
2025-11-17 19:20:42 -0600

    Funciones extendiadas en abogado service

--- src/main/kotlin/usuario/domain/port/Service/AbogadoServicePort.kt
+++ src/main/kotlin/usuario/domain/port/Service/AbogadoServicePort.kt
@@ -1,5 +1,6 @@
 package com.example.usuario.domain.port.Service
 
+import com.example.shared.domain.model.Especialidad
 import com.example.usuario.domain.model.Abogado
 
 interface AbogadoServicePort {
@@ -7,4 +8,13 @@ interface AbogadoServicePort {
     suspend fun getAbogadoById(id: String): Abogado?
     suspend fun getAbogadosByEspecialidad(especialidadId: Int): List<Abogado>
     suspend fun updateAbogado(id: String, abogado: Abogado): Abogado?
+    suspend fun buscarAbogadosPorNombre(nombre: String): List<Abogado>
+    suspend fun getAbogadosByLocalidad(estadoId: Int?, municipioId: Int?): List<Abogado>
+    suspend fun filtrarAbogados(
+        materiaId: Int?,
+        estadoId: Int?,
+        municipioId: Int?,
+        ordenarPorCalificacion: Boolean
+    ): List<Abogado>
+    suspend fun getEspecialidadesByAbogadoId(abogadoId: String): List<Especialidad>
 }
