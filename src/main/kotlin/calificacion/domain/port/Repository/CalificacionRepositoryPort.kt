a0954e886981704a571573979803a7c2b1d1434f
Luissmont <luispm2424@gmail.com>
2025-11-18 22:11:12 -0600

    funciones extendidas para calificacion

--- src/main/kotlin/calificacion/domain/port/Repository/CalificacionRepositoryPort.kt
+++ src/main/kotlin/calificacion/domain/port/Repository/CalificacionRepositoryPort.kt
@@ -2,6 +2,7 @@ package com.example.calificacion.domain.port.Repository
 
 import com.example.calificacion.domain.model.Calificacion
 import com.example.calificacion.domain.model.CalificacionPromedio
+import com.example.calificacion.domain.model.PromediosRespuesta
 
 interface CalificacionRepositoryPort {
     suspend fun findByAbogadoId(abogadoId: String): List<Calificacion>
@@ -9,4 +10,5 @@ interface CalificacionRepositoryPort {
     suspend fun create(calificacion: Calificacion): Calificacion?
     suspend fun getPromediosByAbogado(abogadoId: String): CalificacionPromedio
     suspend fun updateAbogadoCalificacionPromedio(abogadoId: String): Boolean
+    suspend fun getPromediosByRespuestaId(respuestaId: Int): PromediosRespuesta
 }
--- src/main/kotlin/calificacion/domain/port/Service/CalificacionServicePort.kt
+++ src/main/kotlin/calificacion/domain/port/Service/CalificacionServicePort.kt
@@ -3,10 +3,13 @@ package com.example.calificacion.domain.port.Service
 import com.example.calificacion.domain.model.Calificacion
 import com.example.calificacion.domain.model.CalificacionPromedio
 import com.example.calificacion.domain.model.CreateCalificacionRequest
+import com.example.calificacion.domain.model.PromediosRespuesta
 
 interface CalificacionServicePort {
     suspend fun getCalificacionesByAbogadoId(abogadoId: String): List<Calificacion>
     suspend fun getCalificacionById(id: Int): Calificacion?
     suspend fun getPromediosByAbogado(abogadoId: String): CalificacionPromedio
     suspend fun createCalificacion(usuarioId: String, abogadoId: String, request: CreateCalificacionRequest): Calificacion?
+    suspend fun getPromedioGeneralAbogado(abogadoId: String): Double
+    suspend fun getPromediosPorRespuesta(respuestaId: Int): PromediosRespuesta
 }
