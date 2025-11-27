8a2e7e430bdb9e87995c0aca8277c05bf638e14d
JILZXY <sasosiso@gmail.com>
2025-11-26 18:37:32 -0600

    :(

--- src/main/kotlin/shared/config/DatabaseConfig.kt
+++ src/main/kotlin/shared/config/DatabaseConfig.kt
@@ -8,18 +8,20 @@ import java.util.Properties
 
 object DatabaseConfig {
 
-    private val props = Properties().apply {
-        load(File(".env").inputStream())
-    }
+    // --- Definición de variables leyendo directamente de System.getenv() ---
 
-    private val driver = props.getProperty("DB_DRIVER") ?: "org.postgresql.Driver"
-    private val dbHost = props.getProperty("DB_HOST") ?: error("DB_HOST is missing in .env")
-    private val dbPort = props.getProperty("DB_PORT") ?: error("DB_PORT is missing in .env")
-    private val dbName = props.getProperty("DB_NAME") ?: error("DB_NAME is missing in .env")
+    // Si la aplicación fallara sin driver, puedes mantenerlo, sino, elimínalo
+    private val driver = System.getenv("DB_DRIVER") ?: "org.postgresql.Driver"
+
+    // Obtener las variables inyectadas por Systemd (ahora no se busca el archivo .env)
+    private val dbHost = System.getenv("DB_HOST") ?: error("DB_HOST is missing in Systemd Environment")
+    private val dbPort = System.getenv("DB_PORT") ?: error("DB_PORT is missing in Systemd Environment")
+    private val dbName = System.getenv("DB_NAME") ?: error("DB_NAME is missing in Systemd Environment")
+
     private val dbUrl = "jdbc:postgresql://$dbHost:$dbPort/$dbName"
-    private val user = props.getProperty("DB_USERNAME") ?: error("DB_USERNAME is missing in .env")
-    private val pass = props.getProperty("DB_PASSWORD") ?: error("DB_PASSWORD is missing in .env")
 
+    private val user = System.getenv("DB_USERNAME") ?: error("DB_USERNAME is missing in Systemd Environment")
+    private val pass = System.getenv("DB_PASSWORD") ?: error("DB_PASSWORD is missing in Systemd Environment")
     init {
         Class.forName(driver)
     }
